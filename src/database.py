d515934a90ae089b782889fc982d65d080b513f9
Daniel Ferrari <ferrari.dg@gmail.com>
2020-04-12 11:09:09 +0100

    Rename modules to improve meaning.

--- src/database.py
+++ src/database.py
@@ -1,217 +1,145 @@
-"""Functions to deal with database."""
-import csv
-import pathlib
-import sqlite3
-
-from contextlib import contextmanager
-from datetime import date
-from typing import (
-    Optional,
-    Tuple
+"""Module with client commands to the database."""
+from datetime import (
+    datetime,
+    timedelta
 )
 
 import typer
 
-DBFILE = pathlib.Path("database.db")
+from sqlite import (
+    delete_database,
+    export_database,
+    get_number_of_results,
+    get_last_draw_date,
+    get_result_by_date,
+    init_database,
+    insert_new_result
+)
 
-CREATE_EURO_RESULTS_TABLE = """
-    CREATE TABLE euro_results (
-        dt date NOT NULL PRIMARY KEY,
-        n1 int NOT NULL,
-        n2 int NOT NULL,
-        n3 int NOT NULL,
-        n4 int NOT NULL,
-        n5 int NOT NULL,
-        s1 int NOT NULL,
-        s2 int NOT NULL
+from scrapper import (
+    get_euro_millions_result
+)
+
+from utils import (
+    days_between,
+    print_result
+)
+
+
+app = typer.Typer(add_completion=False)
+
+
+@app.command(name='init')
+def initialize(overwrite: bool = typer.Option(False, help='Overwrite existing database.', show_default=True)):
+    """
+    Initialize database.
+
+    If --overwrite is used, the existing database will be deleted.
+    """
+    if overwrite:
+        delete = typer.confirm("Are you sure you want to delete the existing database?")
+        if not delete:
+            raise typer.Abort()
+
+        delete_database()
+        typer.echo("Current database deleted.")
+
+    if init_database():
+        typer.echo("New database created.")
+    else:
+        typer.echo("Database file already exists.")
+
+
+@app.command(name='status')
+def database_status():
+    """
+    Show the database current status.
+
+    Number of results stored |
+    Last draw date stored |
+    Last results stored.
+    """
+    last_draw_date = get_last_draw_date()
+    result = get_result_by_date(last_draw_date)
+    total = get_number_of_results()
+
+    typer.echo(
+        "Results stored: " +
+        typer.style(str(total), fg=typer.colors.GREEN, bold=True)
     )
-"""
 
-CREATE_EURO_LAST_DRAW_TABLE = """
-    CREATE TABLE last_draw (
-        dt date NOT NULL
+    typer.echo(
+        "Last draw date: " +
+        typer.style(f"{last_draw_date}", fg=typer.colors.GREEN, bold=True)
     )
-"""
+
+    typer.echo("Last result:")
+    if result:
+        print_result(result)
+    else:
+        typer.secho("\tNo result found", fg=typer.colors.YELLOW)
 
 
-@contextmanager
-def connect():
-    """Yield a SQLite3 connection.
-
-    With a context manager will, automatically, close the connection.
+@app.command(name='update')
+def update_result(
+    draw_date: datetime = typer.Option(
+        default="2004-02-13",
+        formats=["%Y-%m-%d"],
+        help="Date to get Euromillions draw numbers.",
+        show_default=True
+    )
+):
     """
-    try:
-        connection = sqlite3.connect(
-            DBFILE,
-            detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES
-        )
-        connection.row_factory = sqlite3.Row
-        yield connection
-    finally:
-        connection.close()
+    Update Euromillions result for a given date.
 
-
-def delete_database():
-    """Delete the existing database file."""
-    if DBFILE.exists():
-        DBFILE.unlink()
-
-
-def init_database() -> bool:
-    """Initialize a new, empty, database file.
-
-    Returns
-    -------
-    bool
-        If database already exists it will return False.
+    If --draw_date is not passed, it will update with the date of the first Euromillions result.
     """
-    if DBFILE.exists():
-        return False
+    typer.echo(
+        "Getting results for: " +
+        typer.style(f"{draw_date:%Y-%m-%d}", fg=typer.colors.GREEN, bold=True)
+    )
 
-    create_tables()
-    return True
+    result = get_euro_millions_result(draw_date.date())
 
-
-def export_database(filename: str) -> bool:
-    """Export the results table to CSV file.
-
-    Parameters
-    ----------
-    filename : str
-        File name and path to the CSV file.
-
-    Returns
-    -------
-    bool
-        Returns True if the CSV file was generated.
-    """
-    if not DBFILE.exists():
-        return False
-
-    fn = pathlib.Path(filename)
-
-    with connect() as con:
-        rows = con.execute("SELECT * FROM euro_results").fetchall()
-
-    if rows is None:
-        return False
-
-    with open(fn, "w") as csv_file:
-        csv_writer = csv.writer(csv_file, delimiter=",")
-        csv_writer.writerow([key for key in rows[0].keys()])
-        csv_writer.writerows(rows)
-
-    return True
+    if result:
+        if insert_new_result(draw_date.date(), result):
+            typer.echo("Result saved!")
+            print_result(result)
 
 
-def create_tables():
-    """Create the tables for a new database."""
-    with connect() as con:
-        con.execute(CREATE_EURO_RESULTS_TABLE)
-        con.execute(CREATE_EURO_LAST_DRAW_TABLE)
+@app.command(name='full-update')
+def full_update(
+    max_days: int = typer.Option(
+        default=None,
+        help="Runs the update for a maximun number of days"
+    )
+):
+    """Update Euromillions results from last date stored until today."""
+    start = datetime(2004, 2, 13)
+    current_date = datetime.today()
+
+    last_date = get_last_draw_date()
+    if last_date:
+        start = datetime(last_date.year, last_date.month, last_date.day) + timedelta(days=1)
+
+    for idx, dt in days_between(start, current_date):
+        typer.echo(f"Processing day {idx+1}")
+        update_result(dt)
+        if max_days and idx + 1 == max_days:
+            break
+
+    typer.echo("Update process finished.")
 
 
-def update_last_draw_date():
-    """Update the last draw date.
-
-    The last draw date will be highest date value on the results table.
-    """
-    with connect() as con:
-        row = con.execute("SELECT MAX(dt) AS last FROM euro_results").fetchone()
-        if row:
-            con.execute("DELETE FROM last_draw")
-            con.execute("INSERT INTO last_draw VALUES(?)", (row['last'], ))
-            con.commit()
+@app.command(name='export')
+def export_database_to_csv(filename: str = typer.Option(
+    default="euromillions.csv",
+    help="CSV filename, it can be an absolute path."
+)):
+    """Export all results on the database to a CSV file."""
+    export_database(filename)
+    typer.echo("Database exported.")
 
 
-def get_last_draw_date() -> Optional[date]:
-    """Get the last draw date stored.
-
-    Returns
-    -------
-    Optional[date]
-        The date of tha last draw. Returns None if there is no date.
-    """
-    with connect() as con:
-        row = con.execute("SELECT dt FROM last_draw").fetchone()
-        dt = row['dt'] if row else None
-
-    return dt
-
-
-def get_result_by_date(dt: date) -> Optional[Tuple[int, ...]]:
-    """Get the result for a given date.
-
-    Parameters
-    ----------
-    dt : date
-        Date when the draw happened.
-
-    Returns
-    -------
-    Optional[Tuple[int, ...]]
-        Number from the draw where the last two are the stars.
-        Returns None if the date didn't exists on the table.
-    """
-    query = f"SELECT n1, n2, n3, n4, n5, s1, s2 FROM euro_results where dt = ?"
-
-    with connect() as con:
-        row = con.execute(query, (dt,)).fetchone()
-
-    return tuple(row) if row else None
-
-
-def get_number_of_results() -> int:
-    """Get the total of results stored.
-
-    Returns
-    -------
-    int
-        Number of results stored on the database.
-    """
-    query = "SELECT count(1) AS n from euro_results"
-
-    with connect() as con:
-        row = con.execute(query).fetchone()
-
-    return row['n']
-
-
-def insert_new_result(draw_date: date, result: Tuple[int, ...]) -> bool:
-    """Insert a new Euromillions results into the database.
-
-    Parameters
-    ----------
-    draw_date : date
-        Date of the draw.
-    result : Tuple[List[int], List[int]]
-        Number from the draw where the last two are the stars.
-
-    Returns
-    -------
-    bool
-        Return True if insert was successful, False otherwise.
-    """
-    query = f"INSERT INTO euro_results VALUES({','.join('?' * 8)})"
-
-    try:
-        with connect() as con:
-            con.execute(query, (draw_date, ) + result)
-            con.commit()
-    except sqlite3.IntegrityError:
-        typer.echo(
-            typer.style("ERROR: ", fg=typer.colors.RED, bold=True) +
-            f"Unable save result on database [this date was already collected]."
-        )
-        return False
-    except Exception as e:
-        print(type(e))
-        typer.echo(
-            typer.style("ERROR: ", fg=typer.colors.RED, bold=True) +
-            f"Unable save result on database [{str(e)}]."
-        )
-        return False
-
-    update_last_draw_date()
-
-    return True
+if __name__ == "__main__":
+    app()
